60d4efdbf15dc7c79c20f166d4a6916482cbba80
Abel <s3953018@student.rmit.edu.au>
2022-05-20 22:47:46 +1000

    fixed profile page

---
+++ QRCodeScanner/app/build/generated/source/buildConfig/debug/com/RHAMJET/rhamscan/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.RHAMJET.rhamscan;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.RHAMJET.rhamscan";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
