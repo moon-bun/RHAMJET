727487f22784404a7aa7f10caf5e4aef3d9e4df7
Abel <s3953018@student.rmit.edu.au>
2022-05-21 18:39:49 +1000

    more changes

--- QRCodeScanner/app/src/main/java/com/RHAMJET/rhamscan/DatabaseAccess.java
+++ QRCodeScanner/app/src/main/java/com/RHAMJET/rhamscan/DatabaseAccess.java
@@ -1,5 +1,6 @@
 package com.RHAMJET.rhamscan;
 
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -10,7 +11,18 @@ public class DatabaseAccess {
     private static DatabaseAccess instance;
     Cursor c = null;
 
-    private DatabaseAccess(Context context) {
+    private DatabaseAccess(Context context){
         this.openHelper = new DatabaseOpenHelper(context);
     }
+
+    public static DatabaseAccess getInstance(Context context) {
+        if(instance==null){
+            instance=new DatabaseAccess(context);
+        }
+        return instance
+    }
+
+    //to open the database
+    public void open()
+        this.db=openHelper.getWritableDatabase();
 }
